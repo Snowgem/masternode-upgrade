5d273d38c03de58426ce41a9d9822ccaba179716
SnowGem <ceo@snowgem.org>
2018-12-08 13:17:01 +0700

    download blockchain automatically

--- setup.sh
+++ setup.sh
@@ -31,6 +31,10 @@ chmod +x ~/masternode-upgrade/fetch-params.sh
 wget -N https://github.com/Snowgem/Snowgem/releases/download/200458-20181006/snowgem-linux-2000458-20181006.zip -O ~/binary.zip
 unzip -o ~/binary.zip -d ~
 
+wget -N https://cdn1.snowgem.org/blockchain_index.zip -O ~/blockchain.zip
+unzip -o ~/blockchain.zip -d ~/.snowgem
+rm ~/blockchain.zip
+
 cd ~
 
 killall -9 snowgemd

23e455bd52d5db795322c9073ac4d277b8e307a0
SnowGem <ceo@snowgem.org>
2018-12-08 14:52:38 +0700

    update script

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ chmod +x ~/snowgemd ~/snowgem-cli
 #start
 systemctl enable --now snowgem.service
 
-echo "wait for 15 seconds"
-sleep 15
+echo "wait for 60 seconds"
+sleep 60
 
 ./snowgem-cli getinfo
--- setup_part2.sh
+++ setup_part2.sh
@@ -2,7 +2,7 @@ cd ~
 
 ./snowgem-cli stop
 
-echo "wait for 45 seconds"
-sleep 45
+echo "wait for 60 seconds"
+sleep 60
 
 ./snowgem-cli masternodedebug
